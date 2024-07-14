af88a38d1c418c75acb7624e419450c177cf4152
kero <kerlossony@yahoo.com>
2024-07-14 17:29:43 +0300

    new editing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import Image from "next/image";
-import HomePage from "./blogs/page";
 import Blogs from "./blogs/page";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const navigate = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hello World
+      <div
+        onClick={() => navigate.push('/blogs')}
+        className="z-10 justify-center w-full max-w-5xl items-center font-mono text-sm lg:flex">
+        <Image src='https://images.pexels.com/photos/15740861/pexels-photo-15740861/free-photo-of-couple-at-scenic-viewpoint-overlooking-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image' width={500} height={500} />
       </div>
     </main>
   );
