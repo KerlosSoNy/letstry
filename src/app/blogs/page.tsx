d00efc1e7d8876f7bfc91d769a13674409606bbb
kero <kerlossony@yahoo.com>
2024-07-21 19:07:46 +0300

    new oage

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Blogs() {
     return (
-        <div className="text-3xl font-bold">Blogs Page</div>
+        <div className="text-3xl font-bold" > Blogs Page
+            <Link href={`/blogs/blog/${1}`}>Go To blog number {1}</Link>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const navigate = useRouter()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24" >
       <div
         onClick={() => navigate.push('/blogs')}
         className="z-10 justify-center w-full max-w-5xl items-center font-mono text-sm lg:flex">
         <Image src='https://images.pexels.com/photos/15740861/pexels-photo-15740861/free-photo-of-couple-at-scenic-viewpoint-overlooking-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image' width={500} height={500} />
       </div>
-    </main>
+    </main >
   );
 }
